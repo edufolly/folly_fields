e6faf5323b94bd238d0f9549d6046b04c06bcb14
Eduardo Folly <edufolly@gmail.com>
2020-12-05 22:21:10 -0300

    CPF Field.

---
+++ lib/fields/cpf_field.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:folly_fields/fields/validator_field.dart';
+import 'package:folly_fields/validators/cpf_validator.dart';
+
+///
+///
+///
+class CpfField extends ValidatorField {
+  CpfField({
+    Key key,
+    String validatorMessage = 'Informe o CPF.',
+    String prefix,
+    String label,
+    TextEditingController controller,
+    TextAlign textAlign = TextAlign.start,
+    int maxLength,
+    FormFieldSetter<String> onSaved,
+    String initialValue,
+    bool enabled = true,
+    AutovalidateMode autoValidateMode = AutovalidateMode.disabled,
+    ValueChanged<String> onChanged,
+    FocusNode focusNode,
+    TextInputAction textInputAction,
+    ValueChanged<String> onFieldSubmitted,
+    EdgeInsets scrollPadding = const EdgeInsets.all(20.0),
+    bool enableInteractiveSelection = true,
+    bool filled = false,
+  }) : super(
+          key: key,
+          abstractValidator: CpfValidator(),
+          validatorMessage: validatorMessage,
+          prefix: prefix,
+          label: label,
+          controller: controller,
+          textAlign: textAlign,
+          maxLength: maxLength,
+          onSaved: onSaved,
+          initialValue: initialValue,
+          enabled: enabled,
+          autoValidateMode: autoValidateMode,
+          onChanged: onChanged,
+          focusNode: focusNode,
+          textInputAction: textInputAction,
+          onFieldSubmitted: onFieldSubmitted,
+          autocorrect: false,
+          enableSuggestions: false,
+          textCapitalization: TextCapitalization.none,
+          scrollPadding: scrollPadding,
+          enableInteractiveSelection: enableInteractiveSelection,
+          filled: filled,
+        );
+}
