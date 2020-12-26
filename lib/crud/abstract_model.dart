e7ffbff553d67f5d36dad632da7938e38b7340fe
Eduardo Folly <edufolly@gmail.com>
2020-12-26 14:38:53 -0300

    Creating multi map transformation.

--- lib/crud/abstract_model.dart
+++ lib/crud/abstract_model.dart
@@ -57,4 +57,44 @@ abstract class AbstractModel with Hashable {
   ///
   ///
   String get searchTerm;
+
+  ///
+  ///
+  ///
+  static Map<String, dynamic> fromMultiMap(Map<String, dynamic> map) {
+    Map<String, dynamic> newMap = <String, dynamic>{};
+    for (MapEntry<String, dynamic> entry in map.entries) {
+      _multiMapEntry(entry, newMap);
+    }
+    return newMap;
+  }
+
+  ///
+  ///
+  ///
+  static void _multiMapEntry(
+    MapEntry<String, dynamic> entry,
+    Map<String, dynamic> newMap,
+  ) {
+    List<String> parts = entry.key.split('_');
+    if (parts.length > 1) {
+      Map<String, dynamic> internalMap;
+      if (newMap.containsKey(parts[0])) {
+        internalMap = newMap[parts[0]];
+      } else {
+        internalMap = <String, dynamic>{};
+      }
+
+      String internalKey = parts.getRange(1, parts.length).join('_');
+
+      _multiMapEntry(
+        MapEntry<String, dynamic>(internalKey, entry.value),
+        internalMap,
+      );
+
+      newMap[parts[0]] = internalMap;
+    } else {
+      newMap[entry.key] = entry.value;
+    }
+  }
 }
