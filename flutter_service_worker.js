174b8c0a65acf8c50ceb637e078812ef175c10d7
edufolly <edufolly@users.noreply.github.com>
2022-05-11 12:14:22 +0000

    deploy: e1c09ae57ab2ac5624a08d065505699ca8e9e9c0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "fbe799d407d7a979c65d72b2c31032a2",
-"/": "fbe799d407d7a979c65d72b2c31032a2",
+  "index.html": "4b35cd9e4c06978da6922979cae2f9ea",
+"/": "4b35cd9e4c06978da6922979cae2f9ea",
 "manifest.json": "2eb43e28c2026cad45ee25de8b5933b4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"version.json": "7a7f4addf1dfab4c169d8ee608c71743",
+"version.json": "dd6b7d9f412fd45e1e8f1c22d350a418",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
@@ -23,7 +23,7 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
 "canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
 "canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
-"main.dart.js": "ce07c0b045622666af0f615cfa8bdc2c"
+"main.dart.js": "2ef46bbb810bace684ea3fa649145517"
 };
 
 // The application shell files that are downloaded before a service worker can
