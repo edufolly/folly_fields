3e0d3d5acbee4cf483c4d3f7470e3b18f8374cba
edufolly <edufolly@users.noreply.github.com>
2022-05-12 19:05:01 +0000

    deploy: de9fa6962867ded87644e642dfdc18eeb9efc385

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "c83cb507b5d84e4bd3c1959cc09794c9",
+"main.dart.js": "8cc771d40dfb03bf6fe497892ef24fa0",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
@@ -22,8 +22,8 @@ const RESOURCES = {
 "assets/NOTICES": "d231553c7fa2666d0f5fa87a207f86fc",
 "assets/FontManifest.json": "3ddd9b2ab1c2ae162d46e3cc7b78ba88",
 "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
-"index.html": "ecf385d1142729c4879f74e29977c7e4",
-"/": "ecf385d1142729c4879f74e29977c7e4",
+"index.html": "59990e6a50ddbf962026dd31e4fb8e66",
+"/": "59990e6a50ddbf962026dd31e4fb8e66",
 "manifest.json": "2eb43e28c2026cad45ee25de8b5933b4"
 };
 
