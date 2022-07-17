cb963e82006ffa72f45c85451c42f5f7a45d93de
Eduardo Folly <edufolly@gmail.com>
2022-07-17 17:09:49 -0300

    Adding ChoiceChipField.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:folly_fields/fields/bool_field.dart';
 import 'package:folly_fields/fields/cep_field.dart';
 import 'package:folly_fields/fields/cest_field.dart';
+import 'package:folly_fields/fields/choice_chip_field.dart';
 import 'package:folly_fields/fields/cnae_field.dart';
 import 'package:folly_fields/fields/cnpj_field.dart';
 import 'package:folly_fields/fields/color_field.dart';
@@ -771,11 +772,32 @@ class MyHomePageState extends State<MyHomePage> {
                         onSaved: (Uint8List? newValue) {
                           model.blob = newValue ?? Uint8List(0);
                         },
-                        thumbnailSize: const Size(256,256),
+                        thumbnailSize: const Size(256, 256),
                         showImageThumbnail: true,
                         fileType: FileType.image,
                       ),
                       // [/FileField]
+                    ),
+
+                    CodeLink(
+                      code: code,
+                      tag: 'ChoiceChipField',
+                      source: 'https://github.com/edufolly/folly_fields/'
+                          'blob/main/lib/fields/choice_chip_field.dart',
+                      child:
+                          // [ChoiceChipField]
+                          ChoiceChipField<int>(
+                        label: 'Frutas',
+                        enabled: edit,
+                        items: const <int, String>{
+                          0: 'Banana',
+                          1: 'Maça',
+                          2: 'Laranja',
+                        },
+                        validator: FollyValidators.notNull,
+                        onSaved: (int? value) => model.fruitIndex = value,
+                      ),
+                      // [/ChoiceChipField]
                     ),
 
                     // [/RootCode]
---
+++ lib/fields/choice_chip_field.dart
@@ -0,0 +1,266 @@
+import 'package:flutter/material.dart';
+import 'package:folly_fields/responsive/responsive.dart';
+
+///
+///
+///
+class ChoiceChipField<T> extends FormFieldResponsive<T> {
+  final ChoiceChipFieldController<T>? controller;
+  final Map<T, String>? items;
+
+  ///
+  ///
+  ///
+  ChoiceChipField({
+    String labelPrefix = '',
+    String? label,
+    Widget? labelWidget,
+    this.controller,
+    FormFieldValidator<T?>? validator,
+    super.onSaved,
+    T? initialValue,
+    this.items,
+    super.enabled,
+    AutovalidateMode autoValidateMode = AutovalidateMode.disabled,
+    // TODO(anyone): Implement onChanged
+    // Function(T? value)? onChanged,
+    // ValueChanged<String> onFieldSubmitted,
+    bool filled = false,
+    Color? fillColor,
+    // Widget? hint,
+    // Widget? disabledHint,
+    Color? focusColor,
+    // int elevation = 8,
+    // TextStyle? style,
+    // Widget? icon,
+    // Color? iconDisabledColor,
+    // Color? iconEnabledColor,
+    // double iconSize = 24.0,
+    // bool isDense = true,
+    // bool isExpanded = false,
+    // double? itemHeight,
+    // FocusNode? focusNode,
+    // bool autofocus = false,
+    // Color? dropdownColor,
+    InputDecoration? decoration,
+    EdgeInsets padding = const EdgeInsets.all(8),
+    Color? selectedColor,
+    Color? selectedTextColor,
+    WrapAlignment wrapAlignment = WrapAlignment.spaceEvenly,
+    WrapCrossAlignment wrapCrossAlignment = WrapCrossAlignment.center,
+    super.sizeExtraSmall,
+    super.sizeSmall,
+    super.sizeMedium,
+    super.sizeLarge,
+    super.sizeExtraLarge,
+    super.minHeight,
+    super.key,
+  })  : assert(
+          initialValue == null || controller == null,
+          'initialValue or controller must be null.',
+        ),
+        assert(
+          label == null || labelWidget == null,
+          'label or labelWidget must be null.',
+        ),
+        super(
+          initialValue: controller != null ? controller.value : initialValue,
+          validator: enabled ? validator : (_) => null,
+          autovalidateMode: autoValidateMode,
+          builder: (FormFieldState<T?> field) {
+            ChoiceChipFieldState<T> state = field as ChoiceChipFieldState<T>;
+
+            Color effectiveSelectedColor =
+                selectedColor ?? Theme.of(state.context).colorScheme.primary;
+
+            Color effectiveSelectedTextColor = selectedTextColor ??
+                Theme.of(state.context).colorScheme.onPrimary;
+
+            InputDecoration effectiveDecoration = (decoration ??
+                    InputDecoration(
+                      border: const OutlineInputBorder(),
+                      filled: filled,
+                      fillColor: fillColor,
+                      label: labelWidget,
+                      labelText:
+                          labelPrefix.isEmpty ? label : '$labelPrefix - $label',
+                      counterText: '',
+                      focusColor: focusColor,
+                    ))
+                .applyDefaults(Theme.of(field.context).inputDecorationTheme);
+
+            return Padding(
+              padding: padding,
+              child: InputDecorator(
+                decoration: effectiveDecoration.copyWith(
+                  errorText: enabled ? field.errorText : null,
+                  enabled: enabled,
+                ),
+                isEmpty: state.value == null,
+                child: Wrap(
+                  alignment: wrapAlignment,
+                  crossAxisAlignment: wrapCrossAlignment,
+                  children: state._effectiveController.items!.entries
+                      .map<Widget>(
+                        (MapEntry<T, String> e) {
+                      return ChoiceChip(
+                        label: Text(e.value),
+                        selected:
+                        state._effectiveController.value == e.key,
+                        selectedColor: effectiveSelectedColor,
+                        labelStyle: state._effectiveController.value ==
+                            e.key
+                            ? TextStyle(color: effectiveSelectedTextColor)
+                            : null,
+                        onSelected: (_) {
+                          state._effectiveController.value = e.key;
+                        },
+                      );
+                    },
+                  ).toList(),
+                ),
+              ),
+            );
+          },
+        );
+
+  ///
+  ///
+  ///
+  @override
+  ChoiceChipFieldState<T> createState() => ChoiceChipFieldState<T>();
+}
+
+///
+///
+///
+class ChoiceChipFieldState<T> extends FormFieldState<T> {
+  ChoiceChipFieldController<T>? _controller;
+
+  ///
+  ///
+  ///
+  @override
+  ChoiceChipField<T> get widget => super.widget as ChoiceChipField<T>;
+
+  ///
+  ///
+  ///
+  ChoiceChipFieldController<T> get _effectiveController =>
+      widget.controller ?? _controller!;
+
+  ///
+  ///
+  ///
+  @override
+  void initState() {
+    super.initState();
+    if (widget.controller == null) {
+      _controller = ChoiceChipFieldController<T>(
+        value: widget.initialValue,
+        items: widget.items,
+      );
+    } else {
+      widget.controller!.addListener(_handleControllerChanged);
+    }
+  }
+
+  ///
+  ///
+  ///
+  @override
+  void didUpdateWidget(ChoiceChipField<T> oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.controller != oldWidget.controller) {
+      oldWidget.controller?.removeListener(_handleControllerChanged);
+
+      widget.controller?.addListener(_handleControllerChanged);
+
+      if (oldWidget.controller != null && widget.controller == null) {
+        _controller = ChoiceChipFieldController<T>.fromValue(
+          oldWidget.controller!,
+        );
+      }
+
+      if (widget.controller != null) {
+        setValue(widget.controller!.value);
+
+        if (oldWidget.controller == null) {
+          _controller = null;
+        }
+      }
+    }
+  }
+
+  ///
+  ///
+  ///
+  @override
+  void didChange(T? value) {
+    super.didChange(value);
+    if (_effectiveController.value != value) {
+      _effectiveController.value = value;
+    }
+  }
+
+  ///
+  ///
+  ///
+  @override
+  void reset() {
+    super.reset();
+    setState(() => _effectiveController.value = widget.initialValue);
+  }
+
+  ///
+  ///
+  ///
+  void _handleControllerChanged() {
+    if (_effectiveController.value != value) {
+      didChange(_effectiveController.value);
+    }
+  }
+
+  ///
+  ///
+  ///
+  @override
+  void dispose() {
+    widget.controller?.removeListener(_handleControllerChanged);
+    super.dispose();
+  }
+}
+
+///
+///
+///
+class ChoiceChipFieldController<T> extends ValueNotifier<T?> {
+  Map<T, String>? _items;
+
+  ///
+  ///
+  ///
+  ChoiceChipFieldController.fromValue(ChoiceChipFieldController<T> controller)
+      : _items = controller.items,
+        super(controller.value);
+
+  ///
+  ///
+  ///
+  ChoiceChipFieldController({T? value, Map<T, String>? items})
+      : _items = items,
+        super(value);
+
+  ///
+  ///
+  ///
+  Map<T, String>? get items => _items;
+
+  ///
+  ///
+  ///
+  set items(Map<T, String>? items) {
+    _items = items;
+    super.notifyListeners();
+  }
+}
