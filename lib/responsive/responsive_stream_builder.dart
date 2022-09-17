95a0cf23eaf991c9ca3c0d06cd5edeb1b46a7ac3
Eduardo Folly <edufolly@gmail.com>
2022-09-17 19:36:16 -0300

    Creating ResponsiveFutureBuilder and ResponsiveStreamBuilder.

---
+++ lib/responsive/responsive_stream_builder.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/widgets.dart';
+import 'package:folly_fields/responsive/responsive.dart';
+
+class ResponsiveStreamBuilder<T> extends StreamBuilder<T> with Responsive {
+  @override
+  final int? sizeExtraSmall;
+
+  @override
+  final int? sizeSmall;
+
+  @override
+  final int? sizeMedium;
+
+  @override
+  final int? sizeLarge;
+
+  @override
+  final int? sizeExtraLarge;
+
+  @override
+  final double? minHeight;
+
+  ///
+  ///
+  ///
+  const ResponsiveStreamBuilder({
+    required super.builder,
+    super.initialData,
+    super.stream,
+    this.sizeExtraSmall,
+    this.sizeSmall,
+    this.sizeMedium,
+    this.sizeLarge,
+    this.sizeExtraLarge,
+    this.minHeight,
+    super.key,
+  });
+}
