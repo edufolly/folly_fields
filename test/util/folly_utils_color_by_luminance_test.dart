4db60f6d7aad38b5704ec760aaa52afacc180066
Eduardo Folly <edufolly@gmail.com>
2022-11-05 20:58:58 -0300

    Creating new tests for FollyUtils.

---
+++ test/util/folly_utils_color_by_luminance_test.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/painting.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:folly_fields/util/folly_utils.dart';
+
+///
+///
+///
+void main() {
+  Map<int, int> domain = <int, int>{
+    0xFF000000: 0xFFFFFFFF,
+    0xFFFFFFFF: 0xFF000000,
+  };
+
+  group(
+    'textColorByLuminance',
+    () {
+      for (MapEntry<int, int> input in domain.entries) {
+        test(
+          'Testing ${input.key}',
+          () => expect(
+            FollyUtils.textColorByLuminance(Color(input.key)),
+            Color(input.value),
+          ),
+        );
+      }
+    },
+  );
+}
