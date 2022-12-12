ee537cf4a04c9ae899c67ba95e0e632242cf052e
Eduardo Folly <edufolly@gmail.com>
2022-12-12 09:15:10 -0300

    Updating to FontAwesome 10.3.0.

---
+++ dev/fa_gen.dart
@@ -0,0 +1,51 @@
+// ignore_for_file: avoid_print
+
+import 'package:http/http.dart';
+
+///
+///
+///
+void main() async {
+  String version = '10.3.0';
+
+  Response response = await get(
+    Uri.parse(
+      'https://raw.githubusercontent.com/fluttercommunity/font_awesome_flutter'
+      '/$version/lib/font_awesome_flutter.dart',
+    ),
+  );
+
+  List<String> lines = <String>[];
+
+  List<String> rows = response.body.split('\n');
+
+  for (int i = 0; i < rows.length; i++) {
+    String row = rows[i].trim();
+    if (row.startsWith('static const IconData')) {
+      StringBuffer sb = StringBuffer(row);
+      while (!row.endsWith(';')) {
+        i++;
+        row = rows[i].trim();
+        sb
+          ..write(' ')
+          ..write(row);
+      }
+      lines.add(sb.toString());
+    }
+  }
+
+  RegExp regExp = RegExp('static const IconData (.*)=(.*);');
+
+  for (String line in lines) {
+    RegExpMatch? match = regExp.firstMatch(line);
+    if (match != null) {
+      String? name = match.group(1)?.trim();
+      String? icon = match.group(2)?.trim();
+      if ((icon?.contains('IconData') ?? true) && name != null) {
+        print("'$name': FontAwesomeIcons.$name,");
+      } else {
+        print("'$name': FontAwesomeIcons.$icon,");
+      }
+    }
+  }
+}
---
+++ test/util/ipv4_address_test.dart
@@ -0,0 +1,194 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:folly_fields/util/ipv4_address.dart';
+
+///
+///
+///
+void main() {
+  Map<String, bool> tests = <String, bool>{
+    '': false,
+    ' ': false,
+    'a': false,
+    'a.': false,
+    'a.a': false,
+    'a.a.': false,
+    'a.a.a': false,
+    'a.a.a.': false,
+    '.a': false,
+    '.a.': false,
+    '.a.a': false,
+    '.a.a.': false,
+    '.a.a.a': false,
+    '.a.a.a.': false,
+    'a.a.a.a': false,
+    'A': false,
+    '-1': false,
+    '-1.': false,
+    '-1.-1': false,
+    '-1.-1.': false,
+    '-1.-1.-1': false,
+    '-1.-1.-1.': false,
+    '.-1': false,
+    '.-1.': false,
+    '.-1.-1': false,
+    '.-1.-1.': false,
+    '.-1.-1.-1': false,
+    '.-1.-1.-1.': false,
+    '-1.-1.-1.-1': false,
+    '256': false,
+    '256.': false,
+    '256.256': false,
+    '256.256.': false,
+    '256.256.256': false,
+    '256.256.256.': false,
+    '.256': false,
+    '.256.': false,
+    '.256.256': false,
+    '.256.256.': false,
+    '.256.256.256': false,
+    '.256.256.256.': false,
+    '256.256.256.256': false,
+    '0': false,
+    '0.': false,
+    '0.0': false,
+    '0.0.': false,
+    '0.0.0': false,
+    '0.0.0.': false,
+    '.0': false,
+    '.0.': false,
+    '.0.0': false,
+    '.0.0.': false,
+    '.0.0.0': false,
+    '.0.0.0.': false,
+    '0.0.0.0': true,
+    '255': false,
+    '255.': false,
+    '255.255': false,
+    '255.255.': false,
+    '255.255.255': false,
+    '255.255.255.': false,
+    '.255': false,
+    '.255.': false,
+    '.255.255': false,
+    '.255.255.': false,
+    '.255.255.255': false,
+    '.255.255.255.': false,
+    '255.255.255.255': true,
+    '255.0.0.0': true,
+    '255.255.0.0': true,
+    '255.255.255.0': true,
+    '0.255.255.255': true,
+    '0.0.255.255': true,
+    '0.0.0.255': true,
+    '10.0.0.0': true,
+    '10.255.255.255': true,
+    '100.64.0.0': true,
+    '100.127.255.255': true,
+    '127.0.0.0': true,
+    '127.255.255.255': true,
+    '169.254.0.0': true,
+    '169.254.255.255': true,
+    '172.16.0.0': true,
+    '172.31.255.255': true,
+    '192.0.0.0': true,
+    '192.0.0.255': true,
+    '192.0.2.0': true,
+    '192.0.2.255': true,
+    '192.88.99.0': true,
+    '192.88.99.255': true,
+    '192.168.0.0': true,
+    '192.168.255.255': true,
+    '198.18.0.0': true,
+    '198.19.255.255': true,
+    '198.51.100.0': true,
+    '198.51.100.255': true,
+    '203.0.113.0': true,
+    '203.0.113.255': true,
+    '224.0.0.0': true,
+    '239.255.255.255': true,
+    '233.252.0.0': true,
+    '233.252.0.255': true,
+    '240.0.0.0': true,
+    '255.255.255.254': true,
+  };
+
+  Matcher invalidIpAddress = throwsA(
+    predicate(
+      (dynamic e) => e is ArgumentError && e.message == 'invalidIpAddress',
+    ),
+  );
+
+  group(
+    'IPv4 creating test',
+    () {
+      for (MapEntry<String, bool> entry in tests.entries) {
+        test(
+          'Testing "${entry.key}" for ${entry.value}',
+          () => entry.value
+              ? expect(
+                  Ipv4Address.fromString(entry.key),
+                  isA<Ipv4Address>().having(
+                    (Ipv4Address ip) => ip.toString(),
+                    'Test',
+                    entry.key,
+                  ),
+                )
+              : expect(
+                  () => Ipv4Address.fromString(entry.key),
+                  invalidIpAddress,
+                ),
+        );
+      }
+    },
+  );
+
+  group('IPv4 operations test', () {
+    test(
+      'Testing',
+      () => expect(
+        Ipv4Address.fromString('0.0.0.0') + 1,
+        Ipv4Address.fromString('0.0.0.1'),
+      ),
+    );
+
+    test(
+      'Testing',
+      () => expect(
+        Ipv4Address.fromString('0.0.0.255') + 1,
+        Ipv4Address.fromString('0.0.1.0'),
+      ),
+    );
+
+    test(
+      'Testing',
+      () => expect(
+        () => Ipv4Address.fromString('255.255.255.255') + 1,
+        invalidIpAddress,
+      ),
+    );
+
+    test(
+      'Testing',
+      () => expect(
+        () => Ipv4Address.fromString('0.0.0.0') - 1,
+        invalidIpAddress,
+      ),
+    );
+
+    test(
+      'Testing',
+      () => expect(
+        Ipv4Address.fromString('0.0.1.0') - 1,
+        Ipv4Address.fromString('0.0.0.255'),
+      ),
+    );
+
+    test(
+      'Testing',
+      () => expect(
+        Ipv4Address.fromString('255.255.255.255') - 1,
+        Ipv4Address.fromString('255.255.255.254'),
+      ),
+    );
+  });
+}
