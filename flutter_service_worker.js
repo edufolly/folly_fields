b8925970551a6ee5dec5e6dd1f3d75e3ccd99644
edufolly <edufolly@users.noreply.github.com>
2023-04-01 18:17:48 +0000

    deploy: d209357d11cdc88c6674972cb30af752972bcfad

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
-"main.dart.js": "f7f141c2b69ac9cffaf054798276c170",
+"main.dart.js": "52cebab2b0f2088f088096052fba1400",
 "canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
 "canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
 "canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
@@ -20,8 +20,8 @@ const RESOURCES = {
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/FontManifest.json": "5a32d4310a6f5d9a6b651e75ba0d7372",
 "assets/NOTICES": "8bd7f92186ae779fc0448243f4d080b9",
-"index.html": "8051ebae390252905bb4d8ab25810309",
-"/": "8051ebae390252905bb4d8ab25810309",
+"index.html": "140e8ad5953c949fee361584cca47c62",
+"/": "140e8ad5953c949fee361584cca47c62",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
