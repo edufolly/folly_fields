92e777decdd9e5638a7fe0dd50bb2d44047162d6
edufolly <edufolly@users.noreply.github.com>
2024-09-19 18:33:29 +0000

    deploy: 3e021f13dc05e68a87967f9bbcf18bee6524e701

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2013686381"
+    serviceWorkerVersion: "1665595775"
   }
 });
